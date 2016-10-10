14a3c5243454298b401457019342d35a0a002ab4
Mike Sweetman <mi894527@bmrn.com>
2016-10-10 10:21:40 -0700

    cleaned up render_link_objects

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -47,10 +47,7 @@ module ChartsHelper
     objects = []
     scope = issue_scope(chart)
     if ('0' + chart.group_by_field.to_s).to_i > 0
-      objects = scope.map{ |i| i.custom_field_value(chart.group_by_field.to_i) }.uniq.compact.sort
-      if CustomField.find(chart.group_by_field).multiple
-        objects = objects.flatten.uniq.compact.sort
-      end
+      objects = scope.map{ |i| i.custom_field_value(chart.group_by_field.to_i) }.flatten.uniq.compact.sort
     else
       objects = scope.map{ |i| eval("i." + chart.group_by_field.to_s) }.uniq.compact.sort unless chart.group_by_field.to_s == 'created_on'
     end
